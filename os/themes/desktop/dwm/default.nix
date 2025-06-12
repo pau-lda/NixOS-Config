ad05a22af557e42357a6b3d14a0de38e096eea78
Pauli <pauli_feu@yahoo.de>
2025-06-12 23:18:37 +0200

    override default displaymanager

--- os/themes/desktop/dwm/default.nix
+++ os/themes/desktop/dwm/default.nix
@@ -6,6 +6,7 @@
   services.xserver = {
     enable = true;
     windowManager.dwm.enable = true;
+    displayManager.startx.enable = true; # use startx to override default lightdm
   };
 
   # Configure keymap in X11
