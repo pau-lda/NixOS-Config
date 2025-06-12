bdd43068795211f0d2ec7a750702ca6a3359edb0
Pauli <pauli_feu@yahoo.de>
2025-06-12 23:18:20 +0200

    remove unnecessary dwm config

--- home/programs/applications/dwm.nix
+++ home/programs/applications/dwm.nix
@@ -1,26 +1,7 @@
 {config,lib,pkgs,...}:
 {
 
-        #home.packages = with pkgs; [dwm];
-        xsession = {
-            enable = true;
-            windowManager.command = "dwm";  # Ensures DWM starts for your user
-        };
-
-        /*
-        nixpkgs.overlays = [
-            (self: super: {
-                dwm = super.dwm.overideAttrs(_: {
-                    src = builtins.fetchGit {
-                        url = "https://git.suckless.org/dwm";
-                        ref = "master";
-
-                    }
-                })
-            })
-        ]
-        */
-
+        #services.xserver.displayManager.startx.enable
         #services.xserver.displayManager.lightdm.enable = false;
         #services.xserver.displayManager.startx.enable = true; 
 
