0adf1fb22946071e6e8a19592f072a54d0750f40
Pauli <pauli_feu@yahoo.de>
2025-09-29 23:03:03 +0200

    add hyprland support

--- home/programs/applications/hyprland.nix
+++ home/programs/applications/hyprland.nix
@@ -5,4 +5,9 @@
     # inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprbars
     # "/absolute/path/to/plugin.so"
   #];
+  wayland.windowManager.hyprland.settings = {
+    "$mod" = "SUPER";
+  };
+  wayland.windowManager.hyprland.systemd.variables = ["--all"];
+  xdg.configFile."uwsm/env".source = "${config.home.sessionVariablesPackage}/etc/profile.d/hm-session-vars.sh"; 
 }
--- home/themes/desktop/hyprland/default.nix
+++ home/themes/desktop/hyprland/default.nix
@@ -5,6 +5,7 @@
     ../../../programs/applications/hyprland.nix
     ../../../programs/applications/vscode.nix
     ../../../programs/applications/alacritty.nix
+    ../../../programs/applications/kitty.nix
     ../../../programs/dev/go.nix
     ../../../programs/dev/rust.nix
     ../../../programs/dev/gnumake.nix
